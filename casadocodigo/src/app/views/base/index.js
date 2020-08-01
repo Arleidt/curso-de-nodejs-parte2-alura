f0aff3cbba623869d67993c8da7fcb545b6e8280
arlei duarte <arleidt@yahoo.com.br>
2020-07-31 22:41:47 -0300

    Inicio implementação sessão de autenticação

--- casadocodigo/src/app/views/base/index.js
+++ casadocodigo/src/app/views/base/index.js
@@ -1,6 +1,6 @@
 //Importação de templates, um objeto JavaScript template contendo todos os templates da nossa aplicação. declararemos um módulo a ser exportado, 
-//que na realidade consistirá em um objeto JavaScript contendo todos os templates dentro da pasta "base" - lembrando que um objeto desse tipo é composto 
-//por uma chave e um valor. 
+//que na realidade consistirá em um objeto JavaScript contendo todos os templates dentro da pasta "base" - 
+//lembrando que um objeto desse tipo é composto por uma chave e um valor. 
 
 module.exports = {
     erro404: require('./erros/404.marko'),
